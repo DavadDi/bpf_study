172ef1111ef686db8e1fce4a8336d0ae7c5c28b7
DavadDi <dwh0403@163.com>
2020-08-10 16:25:35 +0800

    update sh

--- replace_img_addr.sh
+++ replace_img_addr.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-
-sed -i "s/https:\/\/www\.do1618\.com\/wp-content\/uploads\/2020\/08/imgs/g" bpf_intro.md
+# mac sed -i "" 
+# 发现 -i 需要带一个字符串，用来备份源文件
+sed -i "" "s/https:\/\/www\.do1618\.com\/wp-content\/uploads\/2020\/08/imgs/g" README.md
